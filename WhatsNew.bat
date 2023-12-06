3dc9400153f0693e7de9f7916f375952bac3531f
yakenohara <38835246+yakenohara@users.noreply.github.com>
2023-12-06 17:50:55 +0900

    add file

---
+++ WhatsNew.bat
@@ -0,0 +1,64 @@
+::<License>------------------------------------------------------------
+::
+:: Copyright (c) 2023 Shinnosuke Yakenohara
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::
+::-----------------------------------------------------------</License>
+
+@echo off
+
+:: <User Setting>------------------------------------------------------
+
+:: 走査対象フォルダ
+::     フルパスで指定します。
+::     `folderPath=` の直後から記載します。 (`=` の直後にスペースは入れません)
+::     パス名にスペースが含まれる場合でもそのまま記載します。
+::      (e.g. `set folderPath=C:\Users\myname\s p a c e`)
+::     デフォルト `%~dp0%` はこのバッチファイルが配置されたディレクトリ
+set folderPath=%~dp0%
+
+:: 走査する深さ
+set /a depth=120
+
+:: -----------------------------------------------------</User Setting>
+
+:: 定数
+set ps1FileName=WhatsNew.ps1
+
+:: 初期化
+set ps1FileFullPath=%~dp0%ps1FileName%
+
+:: 走査対象ディレクトリ文字列の最後の `\` を削除
+echo %folderPath%|findstr \\$ >nul && set folderPath=%folderPath:~0,-1%
+:: Note
+:: `findstr` -> 文字列を正規表現で検索
+::              検索でヒットした場合は、 errorlevel が `0` となる
+::              ヒットしなかった場合は、 errorlevel が `1` となる
+:: https://learn.microsoft.com/ja-jp/windows-server/administration/windows-commands/findstr
+:: `&&`      -> `findstr` 実行で errorlevel == `0` の場合だけ以降のコマンドを実行
+:: `set`     -> 文字列置換を文字位置指定で実行 (以下 `set /?` によるヘルプ抜き出し)
+:: ```
+::     %folderPath:~0,-2%
+:: 
+:: は最後の 2 文字以外のすべてが展開されます。
+:: ```
+
+:: Call powershell
+:: Note
+:: Powershell への第 1 引数にスペースが存在しても OK とするために `"` (ダブルクォート) で括っているが、
+:: 終端の `"` に対するエスケープ `\` は 2 文字記載しないといけない
+:: `powershell` コマンドへの引数内部で変数展開したいので `Bypass` 以降の文字列を `"` で括っているが、
+:: この内部で `"` を使用するためには、 `\` でエスケープしなければならない。 <- それをさらにエスケープするため？
+powershell -ExecutionPolicy Bypass "& \"%ps1FileFullPath%\" \"%folderPath%\" %depth%"
---
+++ WhatsNew.ps1
@@ -0,0 +1,29 @@
+﻿# 引数を変数に格納
+Param(
+	[String]$arg0,
+	[Int]$arg1
+)
+
+# 引数１の内容を表示
+Write-Host $arg0
+# 引数２の内容を表示
+Write-Host $arg1
+
+# 検索対象となる `System.IO.FileInfo` オブジェクトリストを作成
+$obj_finfos = 
+    Get-ChildItem -Path $arg0 -Recurse | # `System.IO.FileInfo` オブジェクトリストを取得
+    Sort-Object -Property FullName # フルパスの名称で sort
+
+# 対象件数が 0 だった場合は終了
+if ($obj_finfos -eq $null){ # 対象件数が 0 だった場合
+    Write-Error "パス `"$arg0`" 内に子項目が存在しません。"
+    return
+}
+
+for ($int_idx = 0 ; $int_idx -lt $obj_finfos.count ; $int_idx++){
+    
+    Write-Host "($($int_idx + 1) of $($obj_finfos.count)) $($obj_finfos[$int_idx].FullName)"
+    
+    
+
+}
