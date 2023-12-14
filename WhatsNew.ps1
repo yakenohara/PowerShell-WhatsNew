e884293bd5d59084dd01a6b43929681aa603b07c
yakenohara <38835246+yakenohara@users.noreply.github.com>
2023-12-14 20:14:13 +0900

    タイムスタンプを取得する相対的深さを指定可能に

--- WhatsNew.ps1
+++ WhatsNew.ps1
@@ -36,6 +36,11 @@
     `-1` の場合は、走査対象ディレクトリが配置された階層、
     `-2` の場合は、走査対象ディレクトリの 1 階層上を意味します。
 
+    .PARAMETER TimeDepth
+    タイムスタンプを取得する相対的深さ (デフォルトは `-Depth` と同じ)
+    `-Depth` パラメータより大きい値の場合は、その差分の分まで深いディレクトリを探索し、最新のタイムスタンプを使用します。
+    `-Depth` より小さい値の場合は、走査対象のディレクトリのタイムスタンプはそのすべての子要素とディレクトリそのものの中から最新のタイムスタンプを使用します。
+
     .PARAMETER FileOnly
     走査対象をファイルに限定します (デフォルト: $false)
 
@@ -60,6 +65,13 @@ Param(
 
     # 階層の深さ
     [System.Int32]$Depth = 0, # 型は signed int (32bit) でないといけない
+    [ValidateScript({
+        if (($_ -ne $null ) -and ($_ -isnot [System.Int32])){ # 型は signed int (32bit) でないといけない
+            return $false
+        } else {
+            return $true
+        }
+    })]$TimeDepth,
 
     # ファイル限定指定
     [switch]$FileOnly,
@@ -69,11 +81,14 @@ Param(
 )
 
 # <引数チェック>
-if ($DirInfo -eq $null) { # 指定されなかった場合
+if ($DirInfo -eq $null) { # `-DirInfo` が指定されなかった場合
     [System.String]$DirInfo = $PSScriptRoot # この .ps1 ファイルが配置されたディレクトリを指定。文字列型。(`Get-ChildItem` のオプション `-Path` が文字列型である必要があるため)
 
 } elseif ($DirInfo -is [System.IO.DirectoryInfo]) { # ディレクトリオブジェクトの指定の場合
     [System.String]$DirInfo = $DirInfo.FullName # パス文字列に変換 (`Get-ChildItem` のオプション `-Path` が文字列型である必要があるため)
+}
+if ($TimeDepth -eq $null){ # `-TimeDepth` が指定されなかった場合
+    $TimeDepth = $Depth # `-Depth` と同じ値を使用
 }
 if ($FileOnly) { # 走査対象をファイルに限定している場合
     $str_FileOpt = " -File"
@@ -99,21 +114,11 @@ if ($Depth -lt 0) {
     [System.UInt32]$uint32_ScanDepth = $Depth # タイムスタンプを検査する階層の深さ
 }
 
-#todo 削除
-Write-Host $DirInfo
-Write-Host $uint32_ScanDepth
-
 # 検索対象となる `System.IO.FileInfo` オブジェクトリストを作成
 $str_GetChildItemCmdlet = "Get-ChildItem -Path `"$DirInfo`"$str_FileOpt$str_DirOpt -Recurse -Depth $uint32_ScanDepth -Force"
 
-#todo 削除
-Write-Host $str_GetChildItemCmdlet
-
 $obj_FofDInfos = Invoke-Expression $str_GetChildItemCmdlet | # `System.IO.FileInfo` オブジェクトリストを取得
     Sort-Object -Property FullName # フルパスの名称で sort
-#todo ファイル指定は `-File` https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-7.4#-file
-# ディレクトリ指定は `-Directory` https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-7.4#-directory
-
 
 # 対象件数が 0 だった場合は終了
 if ($obj_FofDInfos -eq $null){ # 対象件数が 0 だった場合
@@ -128,15 +133,52 @@ if ($obj_FofDInfos -eq $null){ # 対象件数が 0 だった場合
 [System.Management.Automation.PathInfo]$obj_Curdir = Get-Location # カレントディレクトリを一時保存
 Set-Location $DirInfo # 相対パスを取得するためにカレントディレクトリを指定ディレクトリに移動
 
+$int32_tmp = ($TimeDepth - $Depth - 1)
 for ($int32_Idx = 0 ; $int32_Idx -lt $obj_FofDInfos.count ; $int32_Idx++){
     
     # Write-Host "($($int32_Idx + 1) of $($obj_FofDInfos.count)) $($obj_FofDInfos[$int32_Idx].FullName)"
     # Write-Host $obj_FofDInfos[$int32_Idx].GetType().FullName
     [System.String]$str_RelPath = Resolve-Path -Path $obj_FofDInfos[$int32_Idx].FullName -Relative # カレントディレクトリからの相対パスを取得
+    $str_RelPath = $str_RelPath -replace "^\.\\","" # 先頭の `.\` を削除
+    # Write-host $str_RelPath
 
     # https://learn.microsoft.com/ja-jp/dotnet/api/system.text.regularexpressions.regex.matches?view=net-8.0#system-text-regularexpressions-regex-matches(system-string-system-string)
     if (([regex]::Matches($str_RelPath, '\\')).Count -eq $uint32_ScanDepth){
-        Write-Host $str_RelPath
+        
+        if ((-1) -ne $int32_tmp){ # `-TimeDepth` パラメータが指定されていた場合
+
+            IF ($obj_FofDInfos[$int32_Idx].GetType() -eq [System.IO.DirectoryInfo]) { # ディレクトリの場合
+                
+                $str_FullName = $obj_FofDInfos[$int32_Idx].FullName
+                if (0 -le $int32_tmp) {
+                    $str_GetChildItemCmdlet = "Get-ChildItem -Path `"$str_FullName`"$str_FileOpt$str_DirOpt -Recurse -Depth $int32_tmp -Force"
+                } else {
+                    $str_GetChildItemCmdlet = "Get-ChildItem -Path `"$str_FullName`"$str_FileOpt$str_DirOpt -Recurse -Force"
+                }
+                $obj_FofDInfosForTime = Invoke-Expression $str_GetChildItemCmdlet | Sort-Object -Property LastWriteTime -Descending
+                
+                if ($obj_FofDInfosForTime -ne $null){ # 子項目が存在する場合
+
+                    # 現在のディレクトリのタイムスタンプより小項目のタイムスタンプが新しい場合
+                    if (0 -lt (New-TimeSpan -Start $obj_FofDInfos[$int32_Idx].LastWriteTime -End $obj_FofDInfosForTime[0].LastWriteTime).TotalMilliseconds) {
+                        Write-Host $obj_FofDInfos[$int32_Idx].Fullname + $obj_FofDInfosForTime[0].LastWriteTime
+
+                    }else{ # 現在のディレクトリのタイムスタンプが小項目のタイムスタンプが新しい場合
+                        Write-Host $obj_FofDInfos[$int32_Idx].Fullname + $obj_FofDInfos[$int32_Idx].LastWriteTime
+                    }
+
+                } else {
+                    Write-Host $obj_FofDInfos[$int32_Idx].Fullname + $obj_FofDInfos[$int32_Idx].LastWriteTime
+                }
+
+            } else { # ファイルの場合
+                Write-Host $obj_FofDInfos[$int32_Idx].Fullname + $obj_FofDInfos[$int32_Idx].LastWriteTime
+
+            }
+
+        } else { # `-TimeDepth` パラメータが指定されていない場合
+            Write-Host $obj_FofDInfos[$int32_Idx].Fullname + $obj_FofDInfos[$int32_Idx].LastWriteTime
+        }
     }
 
 }
